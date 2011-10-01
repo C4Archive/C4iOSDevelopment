7fd284784cb28bc65d6d79fd1909b18954ab663d
C4 <tkirton@gmail.com>
2011-10-01 17:10:29 -0600

    first commit of an iOS project need to: 1- migrate C4iOS to this project
    Signed-off-by: C4 <tkirton@gmail.com>

---
+++ C4iOSDevelopment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  C4iOSDevelopment
+//
+//  Created by Travis Kirton on 11-10-01.
+//  Copyright (c) 2011 mediart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
